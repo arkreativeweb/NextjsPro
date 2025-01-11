7bdc10459f8f3db28f8b8f04c4d0496c154efeb3
arkreativeweb <arkreativeweb@gmail.com>
2025-01-11 19:01:30 +0700

    ada update

--- next.config.js
+++ next.config.js
@@ -1,17 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['localhost'],
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader'],
-    });
+  experimental: {
+    turbo: {
+      loaders: {}, // Anda bisa tambahkan loader di sini sesuai kebutuhan
+    },
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Hanya tambahkan aturan ini untuk Webpack jika mode server tidak aktif
+      config.module.rules.push({
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      });
+    }
     return config;
   },
-}
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
